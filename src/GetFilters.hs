90668573ed97017c0a2e11b6499453dec2bb4383
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2017-04-13 18:39:30 +0200

    clean up imports

--- src/GetFilters.hs
+++ src/GetFilters.hs
@@ -5,12 +5,15 @@ module GetFilters (
     parseFilter
 ) where
 
-import qualified Data.Map
+import qualified Data.Map (map)
+import Data.Map (empty, intersectionWith, Map, mapWithKey, unionWith)
 import qualified Data.Set
 import Data.List (group, sort, sortBy)
 import Data.Aeson
 
 import Filter
+
+mapMap = Data.Map.map
 
 instance Ord Value where
     compare v1 v2 = compare (show v1) (show v2)
@@ -18,11 +21,8 @@ instance Ord Value where
 rmdups :: (Ord a) => [a] -> [a]
 rmdups = map head . group . sort
 
-sortValues :: [Data.Map.Map String Value] -> Data.Map.Map String [Value]
+sortValues :: [Map String Value] -> Map String [Value]
 sortValues = mapMap rmdups . foldl (unionWith (++)) empty . map (mapMap (:[]))
-    where   empty       = Data.Map.empty
-            unionWith   = Data.Map.unionWith
-            mapMap      = Data.Map.map
 
 number = "Number"
 string = "String"
@@ -47,16 +47,13 @@ prune :: Int -> [a] -> [a]
 prune _ [] = []
 prune n (x:xs) = x : prune n (drop n xs)
 
-filtersFromDataTypes :: Data.Map.Map String [Value] -> Data.Map.Map String [String -> Filter]
+filtersFromDataTypes :: Map String [Value] -> Map String [String -> Filter]
 filtersFromDataTypes values = intersectionWith getTypeFilters dataTypes values
     where   dataTypes           = mapMap getDataType values
-            intersectionWith    = Data.Map.intersectionWith
-            mapMap              = Data.Map.map
 
-bindParametersToFilters :: Data.Map.Map String [String -> Filter] -> [Filter]
+bindParametersToFilters :: Map String [String -> Filter] -> [Filter]
 bindParametersToFilters = foldl (++) [] . mapWithKey applyKey
     where   applyKey    = map . flip ($)
-            mapWithKey  = Data.Map.mapWithKey
 
-getFilters :: [Data.Map.Map String Value] -> [Filter]
+getFilters :: [Map String Value] -> [Filter]
 getFilters = (NullFilter:) . bindParametersToFilters . filtersFromDataTypes . sortValues
