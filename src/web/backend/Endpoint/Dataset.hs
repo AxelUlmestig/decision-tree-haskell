335aa7f5fda568576fb2aec5480068e1b58ae699
axel.ulmestig@gmail.com <axel.ulmestig@gmail.com>
2017-04-23 15:44:25 +0200

    implement train dataset endpoint

--- src/web/backend/Endpoint/Dataset.hs
+++ src/web/backend/Endpoint/Dataset.hs
@@ -3,7 +3,8 @@
 
 module Endpoint.Dataset (
     get,
-    put
+    put,
+    train
 ) where
 
 import Network.HTTP.Types
@@ -14,6 +15,14 @@ import Data.Text (unpack)
 import Data.Aeson
 import qualified Data.ByteString.Lazy as LazyBS
 import qualified Data.Map as Map
+import Control.Arrow
+import qualified Data.ByteString.Lazy.Char8 as C8
+import Data.Maybe (fromMaybe)
+import Data.Map (lookup)
+
+
+import qualified Train
+import DecisionTree
 
 --get :: String -> IO Response
 get setName = do
@@ -37,3 +46,31 @@ put setName requestBody = do
     where   filePath = "./datasets/" ++ (unpack setName) ++ ".json"
             decodeMap = decode :: LazyBS.ByteString -> Maybe (Map.Map String Value)
             ioBody = (fmap encode) . (>>= Map.lookup "dataset") . decodeMap . LazyBS.fromStrict <$> requestBody
+
+train setName requestBody = do
+    maybeTrainVar <- ioBody
+    case maybeTrainVar of
+        Just trainVar -> trainInternal setName trainVar
+        Nothing -> return $ responseBuilder status201 [("Content-Type", "text/html")] (fromString "missing targetvar from body")
+
+    where   decodeMap = decode :: LazyBS.ByteString -> Maybe (Map.Map String String)
+            ioBody = (>>= Map.lookup "targetvar") . decodeMap . LazyBS.fromStrict <$> requestBody
+
+trainInternal setName trainVar = do
+    fileContent <- try . readFile $ setFilePath
+    fileContent' <- stringifyErr fileContent
+    modelOrError <- return $ train' trainVar fileContent'
+    case modelOrError of
+        Left err -> return $
+            responseBuilder status400 [("Content-Type", "text/html")] (fromString err)
+        Right model -> LazyBS.writeFile modelFilePath (encode model) >>
+            (return $ responseBuilder status201 [("Content-Type", "text/html")] (fromString modelName))
+
+    where   setFilePath     = "./datasets/" ++ setName ++ ".json"
+            modelFilePath   = "./models/" ++ modelName ++ ".json"
+            modelName       = setName ++ "_" ++ trainVar
+            stringifyErr    = return . left show :: Either SomeException String -> IO (Either String String)
+            getKey          = fromMaybe (Left "missing targetvar") . fmap Right . Data.Map.lookup "targetvar"
+
+train' :: String -> Either String String -> Either String DecisionTree
+train' key fileContent = fileContent >>= eitherDecode . C8.pack >>= flip Train.train key
--- src/web/backend/Main.hs
+++ src/web/backend/Main.hs
@@ -13,6 +13,7 @@ import qualified Data.ByteString.Char8 as C8
 import Data.Aeson
 import qualified Data.Map as Map
 import qualified Data.ByteString.Lazy as LazyBS
+import qualified Data.Text as T
 
 import qualified Endpoint.Dataset
 import qualified Endpoint.Index
@@ -20,12 +21,12 @@ import qualified Endpoint.Index
 notImplemented = responseBuilder status418 [("Content-Type", "text/text")] (fromString "not implemented")
 
 getResponse :: Request -> IO Response
-getResponse Request{pathInfo="api":"dataset":setName:[], requestMethod="PUT", requestBody=body}     = Endpoint.Dataset.put setName body
-getResponse Request{pathInfo="api":"dataset":setName:[], requestMethod="GET"}                       = Endpoint.Dataset.get setName
-getResponse Request{pathInfo="api":"dataset":setName:"train":[], requestMethod="POST"}              = return $ notImplemented
-getResponse Request{pathInfo="api":"model":modelName:[], requestMethod="GET"}                       = return $ notImplemented
-getResponse Request{pathInfo="api":"model":modelName:"evaluate":[], requestMethod="POST"}           = return $ notImplemented
-getResponse Request{rawPathInfo="/", requestMethod="GET"}                                           = Endpoint.Index.get
+getResponse Request{pathInfo="api":"dataset":setName:[], requestMethod="PUT", requestBody=body}             = Endpoint.Dataset.put setName body
+getResponse Request{pathInfo="api":"dataset":setName:[], requestMethod="GET"}                               = Endpoint.Dataset.get setName
+getResponse Request{pathInfo="api":"dataset":setName:"train":[], requestMethod="POST", requestBody=body}    = Endpoint.Dataset.train (T.unpack setName) body
+getResponse Request{pathInfo="api":"model":modelName:[], requestMethod="GET"}                               = return $ notImplemented
+getResponse Request{pathInfo="api":"model":modelName:"evaluate":[], requestMethod="POST"}                   = return $ notImplemented
+getResponse Request{rawPathInfo="/", requestMethod="GET"}                                                   = Endpoint.Index.get
 getResponse _ = return $ responseBuilder status404 [("Content-Type", "text/text")] (fromString "four oh four")
 
 application :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
