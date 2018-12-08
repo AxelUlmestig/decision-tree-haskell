a003692d09b284659a2c5d3f2baf3a4a10e6af56
Axel Ulmestig <axel.ulmestig@gmail.com>
2018-12-08 11:46:41 +0100

    Refactor statistics logic

--- src/core/StatisticalSignificance.hs
+++ src/core/StatisticalSignificance.hs
@@ -10,7 +10,7 @@ import Prelude                              hiding (fromList, lookup)
 import Statistics.Distribution              (complCumulative)
 import Statistics.Distribution.ChiSquared   (chiSquared, ChiSquared)
 
-statisticallySignificant :: Ord v => Double -> [Map String v] -> String -> (Map String v -> Bool) -> Bool
+statisticallySignificant :: (Ord a, Ord b) => Double -> [Map a b] -> a -> (Map a b -> Bool) -> Bool
 statisticallySignificant limit values key fil =
     if uniqueValues key values < 2
         then False
@@ -18,21 +18,21 @@ statisticallySignificant limit values key fil =
     where   distribution    = getChi2Distribution key values
             chi2            = getChi2 values key fil
 
-getChi2Distribution :: Ord v => String -> [Map String v] -> ChiSquared
+getChi2Distribution :: (Ord a, Ord b) => a -> [Map a b] -> ChiSquared
 getChi2Distribution key = chiSquared . fromIntegral . pred . uniqueValues key
 
-uniqueValues :: Ord v => String -> [Map String v] -> Int
-uniqueValues key = maybe 0 (length . nub) . mapM (lookup key)
+uniqueValues :: (Ord a, Ord b) => a -> [Map a b] -> Int
+uniqueValues key = maybe 0 (length . nub) . traverse (lookup key)
 
-getChi2 :: Ord v => [Map String v] -> String -> (Map String v -> Bool) -> Double
-getChi2 values key fil = foldl (+) 0 $ unionWith f filtered normalized
-    where   f               = \o e -> (o - e)^2 / e
-            raw             = extractValues key values
-            emptyKeys       = fmap (\_ -> 0) raw
-            filtered        = unionWith (+) emptyKeys . extractValues key $ filter fil values
-            normFactor      = foldl (+) 0 filtered / foldl (+) 0 raw
-            normalized      = fmap (* normFactor) raw
+getChi2 :: (Ord a, Ord b) => [Map a b] -> a -> (Map a b -> Bool) -> Double
+getChi2 values key fil  = foldl (+) 0 $ unionWith f filtered normalized
+    where   f           = \o e -> (o - e)^2 / e
+            raw         = extractValues key values
+            emptyKeys   = fmap (\_ -> 0) raw
+            filtered    = unionWith (+) emptyKeys . extractValues key $ filter fil values
+            normFactor  = foldl (+) 0 filtered / foldl (+) 0 raw
+            normalized  = fmap (* normFactor) raw
 
-extractValues :: Ord v => String -> [Map String v] -> Map v Double
-extractValues key = maybe empty toMap . mapM (lookup key)
-    where   toMap   = foldl (\m key -> insertWith (+) key 1 m) empty
+extractValues :: (Ord a, Ord b) => a -> [Map a b] -> Map b Double
+extractValues key = maybe empty toMap . traverse (lookup key)
+    where   toMap = foldl (\m key -> insertWith (+) key 1 m) empty
